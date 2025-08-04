e2bab5d493bdecac2d7bbbbad3bf58c57da26e5b
Sergei Panarin <s.panarin@andersenlab.com>
2025-08-04 16:51:23 +0400

    add logger

--- internal/app/api/config.go
+++ internal/app/api/config.go
@@ -4,10 +4,13 @@ package api
 type Config struct {
 	//Port on which the API server will listen
 	BindAddr string `toml: "bind_addr"` // Bind address for the API server
+	//Logger Level
+	LoggerLevel string `toml: "logger_level"`
 }
 
 func NewConfig() *Config {
 	return &Config{
-		BindAddr: ":8080", // Default port
+		BindAddr:    ":8080", // Default port
+		LoggerLevel: "debug", //Default Logger level
 	}
 }
