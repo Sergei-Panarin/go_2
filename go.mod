54892936b09f27aa49671f55b8d06296c6313646
Sergei Panarin <s.panarin@andersenlab.com>
2025-08-04 16:51:23 +0400

    add logger

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/Sergei-Panarin/go_2/StandardWebServer
 
 go 1.24.3
 
-require github.com/BurntSushi/toml v1.5.0 // indirect
+require (
+	github.com/BurntSushi/toml v1.5.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+)
--- internal/app/api/api.go
+++ internal/app/api/api.go
@@ -1,19 +1,28 @@
 package api
 
+import "github.com/sirupsen/logrus"
+
 // Base API server instance description
 type API struct {
 	//UNEXPORTED FIELD!
 	config *Config
+	logger *logrus.Logger
 }
 
 // API constructor: build base API instance
 func New(config *Config) *API {
 	return &API{
 		config: config,
+		logger: logrus.New(),
 	}
 }
 
 // Start http server/configure loggers, router,database connection and etc.
 func (api *API) Start() error {
+	//Trying to configure Logger
+	if err := api.configureLoggerField(); err != nil {
+		return err
+	}
+	api.logger.Info("Starting api server at port ", api.config.BindAddr)
 	return nil
 }
